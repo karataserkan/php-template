08885d27ba8e15f30aa9e4303748bb566b9213f0
Erkan Karataş <erkan.karatas@btk.gov.tr>
2020-07-22 16:35:29 +0300

    tests updated

--- tests/MockPsr4AutoloaderClass.php
+++ tests/MockPsr4AutoloaderClass.php
@@ -6,4 +6,15 @@ use app\Psr4AutoloaderClass;
 
 class MockPsr4AutoloaderClass extends Psr4AutoloaderClass
 {
+	protected $files = array();
+
+    public function setFiles(array $files)
+    {
+        $this->files = $files;
+    }
+
+    protected function requireFile($file)
+    {
+        return in_array($file, $this->files);
+    }
 }
--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -9,5 +9,11 @@ use app\controllers\TaskController;
 
 final class TaskControllerTest extends TestCase
 {
-
+    public function testCanGetFile()
+    {
+        $controller = new TaskController();
+        $params = ['index.php', 'task/test'];
+        $this->expectOutputString('test');
+        $controller->test($params);
+    }
 }
